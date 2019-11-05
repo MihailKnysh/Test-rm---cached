248505b17e139787e452282e6da497ae26abfcad
MihailKnysh <mishadnepr347@gmail.com>
2019-11-05 07:40:54 +0200

    First commit

---
+++ Factor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Factor
+{
+    class Factor
+    {
+        public int[] FindFactors(int num, out int numfactors)
+        {
+            int i, j;
+            int[] facts = new int[80];
+
+            for (i = 1, j = 0; i < num / 2 + 1; i++)
+            {
+                if (num % i == 0)
+                    facts[j++] = i;
+            }
+            numfactors = j;
+
+            return facts;
+        }
+    }
+    class FindFactor
+    {
+        static void Main(string[] args)
+        {
+            Factor f = new Factor();
+            int numfactors, number = 1000;
+            int[] factors;
+
+            factors = f.FindFactors(number, out numfactors);
+
+            Console.Write("Factors of number {0} are: ", number);
+
+            for (int i = 0; i < numfactors; i++)
+                Console.Write("{0}  ", factors[i]);
+
+            Console.ReadKey();
+        }
+    }
+}
